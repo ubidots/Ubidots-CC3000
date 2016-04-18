1dbb35096d45cecce37d574bb035063486d0185e
metavix <metavix@localhost.localdomain>
2016-04-18 10:47:26 -0500

    fixed library

--- UbidotsCC3000.cpp
+++ UbidotsCC3000.cpp
@@ -1,20 +1,81 @@
+/*
+Copyright (c) 2013-2016 Ubidots.
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Made by Mateo Velez - Metavix for Ubidots Inc
+*/
+
 #include "UbidotsCC3000.h"
 
 /**
  * Constructor.
  */
-Ubidots::Ubidots(char* token){
+Ubidots::Ubidots(char* token) {
     _token = token;
-    maxValues = 4;
+    _dsName = "CC3000";
+    _dsTag = "CC3000";
     currentValue = 0;
-    val = (Value *)malloc(maxValues*sizeof(Value));
+    val = (Value *)malloc(MAX_VALUES*sizeof(Value));
 }
+
 /** 
  * This function is to get value from the Ubidots API
  * @arg id the id where you will get the data
  * @return num the data that you get from the Ubidots API
  */
-float Ubidots::getValue(char* id){
+float Ubidots::getValueWithDatasource(char* dsTag, char* idName) {
+  float num;
+  int i = 0;
+  char* allData = (char *) malloc(sizeof(char) * 250);
+  String response;
+  uint8_t bodyPosinit;
+  sprintf(allData, "Particle/1.0|LV|%s|%s:%s|end", _token, dsTag, idName);
+  _client.connect(ip, PORT);
+    if (_client.connected()) {
+        Serial.println("Geting your variable");
+        _client.fastrprint(allData);
+    } else {
+        Serial.println(F("Connection failed"));  
+        currentValue = 0;  
+        return NULL;
+    }
+    unsigned long lastRead = millis();
+    while (_client.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
+        while (_client.available()) {
+            char c = _client.read();
+            response += c;
+        }
+    }
+    bodyPosinit = 3 + response.indexOf("OK|");
+    response = response.substring(bodyPosinit);
+    num = response.toFloat();
+    currentValue = 0;
+    _client.stop();
+    free(allData);
+    return num;
+}
+
+/** 
+ * This function is to get value from the Ubidots API
+ * @arg id the id where you will get the data
+ * @return num the data that you get from the Ubidots API
+ */
+float Ubidots::getValue(char* id) {
 	float num;
 	int i = 0;
     String raw;
@@ -48,51 +109,82 @@ float Ubidots::getValue(char* id){
     _client.close();
     return num;
 }
+void Ubidots::add(char *variable_id, float value) {
+  add(variable_id, value, NULL);
+}
 /**
  * Add a value of variable to save
  * @arg variable_id variable id to save in a struct
  * @arg value variable value to save in a struct
  */
-void Ubidots::add(char *variable_id, double value){
-  (val+currentValue)->id = variable_id;
-  (val+currentValue)->value_id = value;
+void Ubidots::add(char *variable_id, float value, char *ctext1) {
+  (val+currentValue)->idName = variable_id;
+  (val+currentValue)->idValue = value;
   currentValue++;
-  if(currentValue>maxValues){
-  	Serial.println("You are sending more than 5 consecutives variables, you just could send 5 variables. Then other variables will be deleted!");
-  	currentValue = maxValues;
+  if (currentValue > MAX_VALUES) {
+        currentValue = MAX_VALUES;
   }
 }
 /**
  * Send all data of all variables that you saved
  * @reutrn true upon success, false upon error.
  */
-bool Ubidots::sendAll(){
+bool Ubidots::sendAll() {
     int i;
-    char vals[10];
-    _client.connect(ip, PORT);
-    // Next for is to calculate the lenght of the data that you will send
-    if (_client.connected()) {
-    	Serial.println("Sending your variables");
-        _client.fastrprint(F("CC3000:POST:"));
-        _client.fastrprint(_token);
-        _client.fastrprint(F(":"));
-        for(i=0;i<currentValue;){
-             dtostrf((val+i)->value_id,10, 5, vals);     
-            _client.fastrprint((val+i)->id);
-            _client.fastrprint(",");
-            _client.fastrprint(vals);
-            _client.fastrprint("&"); 
-        i++;
-        }
-        _client.fastrprint(F(":CC3000"));
+    String allData;
+    if (_dsName == "CC3000") {
+        allData = USER_AGENT; 
+        allData += "|POST|";
+        allData += _token;
+        allData += "|";
+        allData += _dsTag;
+        allData += "=>";
+        //sprintf(allData, "%s|POST|%s|%s=>", USER_AGENT, _token, _dsTag);
     } else {
-        Serial.println(F("Connection failed"));  
-        currentValue = 0;  
+        allData = USER_AGENT; 
+        allData += "|POST|";
+        allData += _token;
+        allData += "|";
+        allData += _dsTag;
+        allData += ":";
+        allData += _dsName;
+        allData += "=>";
+        //sprintf(allData, "%s|POST|%s|%s:%s=>", USER_AGENT, _token, _dsTag, _dsName);
+    }
+    for (i = 0; i < currentValue; ) {
+        allData += (val + i)->idName;
+        allData += ":";
+        allData += String((val + i)->idValue,2);
+        //sprintf(allData, "%s%s:%f", allData, (val + i)->idName, (val + i)->idValue);
+        i++;
+        if (i < currentValue) {
+            allData += ",";
+            //sprintf(allData, "%s,", allData);
+        }
+    }
+    allData += "|end";
+    //sprintf(allData, "%s|end", allData);
+#ifdef DEBUG_UBIDOTS
+    Serial.println(allData);
+#endif
+    _client.connect(ip, PORT);
+    if (_client.connected()) {        // Connect to the server
+#ifdef DEBUG_UBIDOTS
+        Serial.println("Client connected"); 
+#endif
+        _client.print(allData);
+        Serial.println("envie");
+    } else {
+        // if you couldn't make a connection:
+        Serial.println("connection failed");
+        Serial.println();
+        Serial.println("disconnecting.");
+        currentValue = 0;
+        _client.stop();
+        _client.close();
         return false;
     }
-
-    /* Read data until either the connection is closed, or the idle timeout is reached. */ 
-
+    while (!_client.available());
     unsigned long lastRead = millis();
     while (_client.connected() && (millis() - lastRead < IDLE_TIMEOUT_MS)) {
         while (_client.available()) {
@@ -102,17 +194,19 @@ bool Ubidots::sendAll(){
         }
     }
     Serial.println();
+    _client.stop();
     _client.close();
     currentValue = 0;
+    //free(allData);
     return true;
 }
+
 /**
  * Initialize the cc3000 WiFi
  */
-void Ubidots::initialize(){
+void Ubidots::initialize() {
     Serial.println(F("\nInitializing..."));
-    if (!cc3000.begin())
-    {
+    if (!cc3000.begin()) {
         Serial.println(F("Couldn't begin()! Check your wiring?"));
         while(1);
     }
@@ -123,7 +217,7 @@ void Ubidots::initialize(){
  * @arg pass the pass of the WiFi
  * @arg security 
  */
-void Ubidots::wifiConnection(char* ssid, char* pass, uint8_t security){
+void Ubidots::wifiConnection(char* ssid, char* pass, uint8_t security) {
     Serial.print(F("\nAttempting to connect to ")); Serial.println(ssid);
     if (!cc3000.connectToAP(ssid, pass, security)) {
         Serial.println(F("Failed!"));
--- UbidotsCC3000.h
+++ UbidotsCC3000.h
@@ -1,3 +1,25 @@
+/*
+Copyright (c) 2013-2016 Ubidots.
+Permission is hereby granted, free of charge, to any person obtaining
+a copy of this software and associated documentation files (the
+"Software"), to deal in the Software without restriction, including
+without limitation the rights to use, copy, modify, merge, publish,
+distribute, sublicense, and/or sell copies of the Software, and to
+permit persons to whom the Software is furnished to do so, subject to
+the following conditions:
+The above copyright notice and this permission notice shall be
+included in all copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+Made by Mateo Velez - Metavix for Ubidots Inc
+*/
+
 #ifndef __UbidotsCC3000_
 #define __UbidotsCC3000_
 #define DEBUG_UBIDOTS
@@ -16,33 +38,42 @@
 // On an UNO, SCK = 13, MISO = 12, and MOSI = 11
 
 #define IDLE_TIMEOUT_MS  3000      // Amount of time to wait (in milliseconds) 
-#define PORT 9001
+#define PORT 9010
 ////////////////////////////////////// Ubidots parameters
 #define WEBSITE "translate.ubidots.com"
+#define USER_AGENT "CC3000/1.0"
+
+#define DEBUG_UBIDOTS
+#define MAX_VALUES 3
 
 typedef struct Value {
-  char  *id;
-  float value_id;
+  char *idName;
+  float idValue;
 } Value;
 
 class Ubidots{
-    private:
+ private:
         Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT,
         SPI_CLOCK_DIVIDER); // you can change this clock speed
         char* _token;
+        char* _dsTag;
+        char* _dsName;
         uint8_t maxValues;
         uint8_t currentValue;
         Value * val;
         uint32_t ip;
         void getIpWebSite();
-        Adafruit_CC3000_Client _client;         
-
-    public:
+        Adafruit_CC3000_Client _client;
+ public:
         Ubidots(char* token);
         void initialize();
+        bool setDatasourceName(char* dsName);
+        bool setDatasourceTag(char* dsTag);
         float getValue(char* id);
-        void add(char *variable_id, double value);
+        void add(char *variable_id, float value);
+        void add(char *variable_id, float value, char* ctext);
         void wifiConnection(char* ssid, char* pass, uint8_t security);
-        bool sendAll();        
+        bool sendAll();
+        float getValueWithDatasource(char* dsName, char* idName);
 };
 #endif

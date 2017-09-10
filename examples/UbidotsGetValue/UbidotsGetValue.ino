f9cad284ba990a888692efabb936491ca5d2a51a
Maria Hernandez <maria@ubidots.com>
2017-09-10 14:44:29 -0500

    examples updated

--- examples/UbidotsGetValue/UbidotsGetValue.ino
+++ examples/UbidotsGetValue/UbidotsGetValue.ino
@@ -1,30 +1,42 @@
+/********************************
+ * Libraries included
+ *******************************/
 #include <UbidotsCC3000.h>
 #include <Adafruit_CC3000.h>
 #include <ccspi.h>
 #include <SPI.h>
 #include <string.h>
 
+/********************************
+ * Constants and objects
+ *******************************/
 #define WLAN_SSID       "OpenWRT"  // Your WiFi SSID, cannot be longer than 32 characters!
 #define WLAN_PASS       "Your_pass_here"  // Replace it with your WiFi pass
 // Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
 #define WLAN_SECURITY   WLAN_SEC_WPA2
-
+/* Assigns the Ubidots parameters */
 #define TOKEN "Your_token_here"  // Replace it with your Ubidots token
-#define ID "Your_id_here" // Replace it with your Ubidots' variable ID
+#define DEVICE_LABEL "Your_id_here" // Replace it with your Ubidots' device label
+#define VARIABLE_LABEL "Your_id_here" // Replace it with your Ubidots' variable label
 
+/* initialize the instance */
 Ubidots client(TOKEN);
 
+/********************************
+ * Main Functions
+ *******************************/
 void setup() {
   Serial.begin(115200);
   client.initialize();
   client.wifiConnection(WLAN_SSID, WLAN_PASS, WLAN_SECURITY);
   //client.setDebug(true); // Uncomment this line to set DEBUG on
-
 }
 
 void loop() {
-  float value = client.getValue(ID);
-  Serial.print("Value: ");
-  Serial.prinln(value);
+  /* Getting the last value from a variable */
+  float value = client.getValue(DEVICE_LABEL, VARIABLE_LABEL);
+  /* Print the value obtained */
+  Serial.print("The value obteined is: ");
+  Serial.println(value);
   delay(5000);
 }
--- examples/UbidotsSaveMultiValues/UbidotsSaveMultiValues.ino
+++ examples/UbidotsSaveMultiValues/UbidotsSaveMultiValues.ino
@@ -1,33 +1,52 @@
+/********************************
+ * Libraries included
+ *******************************/
 #include <UbidotsCC3000.h>
 #include <Adafruit_CC3000.h>
 #include <ccspi.h>
 #include <SPI.h>
 #include <string.h>
-//#include "utility/debug.h"  // Uncomment this line to set DEBUG on
 
+/********************************
+ * Constants and objects
+ *******************************/
 #define WLAN_SSID       "OpenWRT"  // Your WiFi SSID, cannot be longer than 32 characters!
 #define WLAN_PASS       "Your_pass_here"  // Replace it with your WiFi pass
 // Security can be WLAN_SEC_UNSEC, WLAN_SEC_WEP, WLAN_SEC_WPA or WLAN_SEC_WPA2
 #define WLAN_SECURITY   WLAN_SEC_WPA2
-
+/* Assigns the Ubidots parameters */
 #define TOKEN "Your_token_here"  // Replace it with your Ubidots token
-#define VARIABLE_LABEL_1 "Your_variableLabel_here" // Replace it with your Ubidots' variable label
-#define VARIABLE_LABEL_2 "Your_variableLabel_here" // Replace it with your Ubidots' variable label
+#define VARIABLE_LABEL_1 "temperature" // Assign the Ubidots variable label
+#define VARIABLE_LABEL_2 "humidity" // Assign the Ubidots variable label
+#define VARIABLE_LABEL_3 "pressure" // Assign the Ubidots variable label
+#define VARIABLE_LABEL_4 "current" // Assign the Ubidots variable label
+#define VARIABLE_LABEL_5 "voltage" // Assign the Ubidots variable label
 
+/* initialize the instance */
 Ubidots client(TOKEN);
 
+/********************************
+ * Main Functions
+ *******************************/
 void setup() {
   Serial.begin(115200);
   client.initialize();
   client.wifiConnection(WLAN_SSID, WLAN_PASS, WLAN_SECURITY);
+  //client.setDeviceLabel("my-new-device"); // Uncomment this line to change the default device name
   //client.setDebug(true); // Uncomment this line to set DEBUG on
 }
 
 void loop() {
+   /* Sensors readings */
   float value = analogRead(A0);
-  float value2 = analogRead(A1);
-
+ /* Sending values to Ubidots */
   client.add(VARIABLE_LABEL_1,value);
-  client.add(VARIABLE_LABEL_2,value2);
+  client.add(VARIABLE_LABEL_2,value);
   client.sendAll();
+   /* Sending more than 5 variables */
+  client.add(VARIABLE_LABEL_3,value);
+  client.add(VARIABLE_LABEL_4,value);
+  client.add(VARIABLE_LABEL_5,value);
+  client.sendAll();
+  delay(5000);
 }
